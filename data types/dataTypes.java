69ef731192a86bb1f0eadb6b38b4e2b62620dae9
binaryThinker <114472374+ibarraaa@users.noreply.github.com>
2025-12-18 09:58:19 +0800

    Add files via upload

---
+++ data types/dataTypes.java
@@ -0,0 +1,74 @@
+//package data types;
+
+public class dataTypes {
+   public static void main(String[] args){
+
+    System.out.println("Java Data Types");
+
+    int myNum = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hello";
+
+        // primitive number
+    short intShort = 5000;
+    int intt = 100000;
+    long intLong = 15000000000L;
+    byte intByte = -128;
+    
+    float ff = 424.0f;
+    double fDouble = 333.44d;
+
+    // boolean data types
+    boolean Student = true;
+    boolean isOnline = false;
+
+    System.out.println(Student);
+    System.out.println(isOnline);
+
+
+
+    // characters
+
+    char myGrade = 'A';
+    System.out.println(myGrade);
+
+        char numSign = 35, ampersand = 38, space = 32;
+       // System.out.println(numSign, space, ampersand);
+    // real life 
+
+
+
+    int items = 50;
+    float costPerItem = 9.99f;
+    float totalCost = items * costPerItem;
+    char currency = '$';
+
+    System.out.println("Number of items: " + items);
+    System.out.println("Cost per item: " + costPerItem + currency);
+    System.out.println("Total cost = " + totalCost + currency);
+
+
+    // var
+    var x = 0;
+    System.out.println(x);
+
+    var myNum2 = 67;
+    var myDouble2 = 9.09;
+    var plate = "PLU 355";
+
+    System.out.println( myDouble2 );
+      System.out.println(myNum2 );
+      System.out.println( plate);
+    
+    /*
+        var x; // Error
+        var x = 5;  // OK
+    */
+
+
+
+
+    }
+}
