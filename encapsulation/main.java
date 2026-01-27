a5a6d14a3cf1c451ee9a27235760cee24545596f
binaryThinker <114472374+ibarraaa@users.noreply.github.com>
2026-01-27 15:08:08 +0800

    Add files via upload

---
+++ encapsulation/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+package encapsulation;
+public class Main{
+    public static void Main(String[] args){
+
+        Encapsulation objPerson = new encapsulation();
+
+
+        objPerson.setName("Hiruki");
+        objPerson.setCivil("SadBoi");
+        System.out.println(objPerson.getName());
+        System.out.println(objPerson.getCivil());
+
+}
---
+++ encapsulation/second.java
@@ -0,0 +1,21 @@
+public class encapsulation{
+    private String name;
+        private String civil;
+
+        public String  getName(){
+            return name;
+        }
+
+        public String  getCivil(){
+            return civil;
+        }
+
+
+        public void setName(String newName){
+            this.name = newName;
+        }
+
+         public void setCivil(String newCivil){
+            this.civil = newCivil;
+        }
+    }
